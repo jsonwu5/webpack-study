2731fcceee5a7d05fb7a4bc17672b6f1062acf81
Jason <jsonwu@qq.com>
2018-09-15 17:01:28 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
-import _ from 'lodash';
-import printMe from './print';
-import './styles.css'
+import { cube } from './math.js';
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  var btn = document.createElement('pre');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
 
 document.body.appendChild(component());
-let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
-
-if(module.hot){
-  module.hot.accept('./print.js', function () {
-    console.log('Accepting the updated printMe module!');
-    element = component(); // 重新染页面后，component 更新 click 事件处理
-    document.body.appendChild(element);
-  })
-}
